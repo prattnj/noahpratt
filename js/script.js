3e5e83b1917e1776e2630b1bf4040410627a9ce3
Noah Pratt <prattnj@gmail.com>
2023-07-17 14:58:09 -0600

    test

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import {getCustomPentagonalPrism} from "./shapes"
 
-// SET UP RENDERER (test)
+// SET UP RENDERER
 const renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true
 renderer.setClearColor(0x1a1e1f)
